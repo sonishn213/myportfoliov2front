410c6649a290917c46c9044f1ff5c5f04e52e64f
Sonish <sonishn213@gmail.com>
2022-07-21 00:14:37 +0530

    changed words

--- src/components/Sections/HomePage/About/About.js
+++ src/components/Sections/HomePage/About/About.js
@@ -13,7 +13,7 @@ const About = () => {
             humour, or randomised words which don't look even slightly
             believable. If you are going to use a passage of Lorem Ipsum, you
             need to be sure there isn't anything embarrassing hidden in the
-            middle of text.
+            middle of sonish.
           </p>
         </div>
       </div>
